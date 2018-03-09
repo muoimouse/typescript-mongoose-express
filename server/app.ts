e261a8342bf104c5a516a47a6b0b0fdeb5ae3e50
muoimouse@gmail.com <0p0p1wwm@github>
2018-03-09 21:23:25 +0700

    update

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,9 @@ import { APIDocsRouter } from "./routes/swagger";
 
 const app = express();
 
+const serverErrorStatus = 500;
+const portDefault = 3000;
+
 app.use(json());
 app.use(urlencoded({
   extended: true
@@ -18,21 +21,20 @@ app.get("/", (request: express.Request, response: express.Response) => {
 
   response.json({
     name: "Express application"
-  })
+  });
 });
-
 app.use((err: Error & { status: number }, request: express.Request, response: express.Response, next: express.NextFunction): void => {
 
-  response.status(err.status || 500);
+  response.status(err.status || serverErrorStatus);
   response.json({
     error: "Server error"
-  })
+  });
 });
 
 app.use("/api", PostRouter.routes());
 app.use("/api", new AuthorRouter().getRouter());
 app.use("/api/swagger", new APIDocsRouter().getRouter());
-app.use("/docs", express.static(path.join(__dirname, './assets/swagger')));
+app.use("/docs", express.static(path.join(__dirname, "./assets/swagger")));
 
 const server: http.Server = app.listen(process.env.PORT || 3000);
 
